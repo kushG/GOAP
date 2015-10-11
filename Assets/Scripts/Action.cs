0a4f3953aa8dace1be64b491f396832e84b99db2
unknown <kag4380@IGMGRAD-16.main.ad.rit.edu>
2015-10-10 21:15:58 -0400

    First commit

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class Action : MonoBehaviour {
+
+	// Character who is examining the clue(Scientist, Historian, Diplomat or Maverick)
+	public GameObject character;
+
+	private HashSet<KeyValuePair<string, object>> preconditions;
+	private HashSet<KeyValuePair<string, object>> effects;
+
+	public bool inRange = false;
+
+
+	//Move towards the clue to examine the clue
+	public abstract void moveTowards();
+
+
+	// Return true if an action has been performed
+	public abstract bool performAction();
+
+	public bool isInRange(){
+		return inRange;
+	}
+	
+
+	public void addPreconditions(string key, object value){
+		preconditions.Add(new KeyValuePair<string, object>(key, value));
+	}
+
+	public void removePreconditions(string key){
+		foreach(KeyValuePair<string, object> kvp in preconditions){
+			if(kvp.Key.Equals(key)){
+				preconditions.Remove(kvp);
+			}
+		}
+	}
+
+	public void addEffects(string key, object value){
+		effects.Add(new KeyValuePair<string, object >(key, value));
+	}
+
+	public void removeEffects(string key){
+		foreach(KeyValuePair<string, object> kvp in effects){
+			if(kvp.Key.Equals(key)){
+				effects.Remove(kvp);
+			}
+		}
+	}
+
+	public HashSet<KeyValuePair<string, object>> Preconditions {
+		get {
+			return preconditions;
+		}
+	}
+	
+	public HashSet<KeyValuePair<string, object>> Effects {
+		get {
+			return effects;
+		}
+	}
+
+}
