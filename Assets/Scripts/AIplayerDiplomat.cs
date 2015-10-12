040e37aae221078af6ad5caad2435deb0074c2d4
unknown <kag4380@IGMGRAD-16.main.ad.rit.edu>
2015-10-12 19:34:13 -0400

    Adding player data

--- Assets/Scripts/AIplayerDiplomat.cs
+++ Assets/Scripts/AIplayerDiplomat.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class ExamineAclueAction : MonoBehaviour {
+public class AIplayerDiplomat : MonoBehaviour {
+
+	public int actionPoints = 2;
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/FiniteStateMachine.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FiniteStateMachine : MonoBehaviour {
+	public GameObject[] clues = new GameObject[3];
+	public GameObject aiPlayer;
+	public string state;
+
+
+	public bool IdleState(){
+		state = "Idle";
+		movetowardsClueState();
+		return true;
+
+	}
+
+	public bool movetowardsClueState(){
+		state = "moveTowardsClue";
+		float minDistance = 0;
+		GameObject nearestClue;
+		foreach(GameObject clue in clues){
+			if(nearestClue == null){
+				nearestClue = clue;
+				minDistance = (clue.transform.position - aiPlayer.transform.position).magnitude;
+			}
+			else{
+				float distance = (clue.transform.position - aiPlayer.transform.position).magnitude;
+				if(distance < minDistance){
+					minDistance = distance;
+					nearestClue = clue;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public bool solveClue(){
+		state = "solveClue";
+		return true;
+
+	}
+
+}
